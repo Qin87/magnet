5a239be8479eb6d74ce635331ecd70a3bbf7ec09
Qin <qj2004@hw.ac.uk>
2024-01-30 22:26:11 +0000

    2

--- args.py
+++ args.py
@@ -5,15 +5,15 @@ def parse_args():
 
 
     parser.add_argument('--IsDirectedData', type=bool, default=True, help='the dataset is directed graph')
-    parser.add_argument('--AugDirect', type=int, default=2311, help='0 for noAug, 1 for one direction, 2 for bidirection aug edges, '
+    parser.add_argument('--AugDirect', type=int, default=4, help='0 for noAug, 1 for one direction, 2 for bidirection aug edges, '
                                                                  '4 for bidegree and bidirection, 20 for my bidegree(best), 21 for graphSHA bidegree, 2311 is trainmask use row-degree instead of 231 use col-deg')
-    parser.add_argument('--GPUdevice', type=int, default=2, help='device')
+    parser.add_argument('--GPUdevice', type=int, default=0, help='device')
     parser.add_argument('--seed', type=int, default=100, help='seed')
     parser.add_argument('--undirect_dataset', type=str, choices=['Cora', 'CiteSeer', 'PubMed', 'Amazon-Photo', 'Amazon-Computers', 'Coauthor-CS'], default='Cora', help='dataset name')
-    parser.add_argument('--Direct_dataset', type=str, default='cora_ml/', help='dgl/cora, citeseer_npz/ , cora_ml/,  ')
+    parser.add_argument('--Direct_dataset', type=str, default='citeseer_npz/', help='dgl/cora, citeseer_npz/ , cora_ml/,  ')
     # parser.add_argument('--data_path', type=str, default='datasets/', help='data path')
     parser.add_argument('--imb_ratio', type=float, default=100, help='imbalance ratio')
-    parser.add_argument('--net', type=str, choices=['GCN', 'GAT', 'SAGE'], default='SAGE', help='GNN bachbone')
+    parser.add_argument('--net', type=str, choices=['GCN', 'GAT', 'SAGE'], default='GAT', help='GNN bachbone')
     parser.add_argument('--n_layer', type=int, default=2, help='the number of layers')
     parser.add_argument('--feat_dim', type=int, default=64, help='feature dimension')
     parser.add_argument('--warmup', type=int, default=5, help='warmup epoch')
