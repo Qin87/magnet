05af42362db7b35c4189d7d365184b92ce42afa5
Qin87 <qinjiang19871018@gmail.com>
2024-02-02 10:50:19 +0000

    had cpu.sh

--- args.py
+++ args.py
@@ -3,10 +3,12 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--IsDirectedData', action='store_true', help='The dataset is a directed graph')
+    # parser.add_argument('--IsDirectedData', action='store_true', help='The dataset is a directed graph')
+    parser.add_argument('--IsDirectedData', action='store_false', help='The dataset is a directed graph')
     # parser.add_argument('--IsDirectedData', type=bool, default=True, help='the dataset is directed graph')
-    parser.add_argument('--AugDirect', type=int, default=1, help='0 for noAug, 1 for one direction, 2 for bidirection aug edges, '
+    parser.add_argument('--AugDirect', type=int, default=23,  help='0 for noAug, 1 for one direction, 2 for bidirection aug edges, '
                                                                  '4 for bidegree and bidirection, 20 for my bidegree(best), 21 for graphSHA bidegree, 2311 is trainmask use row-degree instead of 231 use col-deg')
+    parser.add_argument('--net', type=str, default='GAT', help='GNN bachbone, chosen from GCN, GAT, SAGE, APPNP, Cheb, DIG, SymDiGCN, GIN')
     parser.add_argument('--GPUdevice', type=int, default=1, help='device')
     parser.add_argument('--seed', type=int, default=100, help='seed')
     parser.add_argument('--undirect_dataset', type=str, choices=['Cora', 'CiteSeer', 'PubMed', 'Amazon-Photo', 'Amazon-Computers', 'Coauthor-CS'], default='Cora', help='dataset name')
@@ -15,7 +17,6 @@ def parse_args():
                                                                               'WebKB/Cornell, WebKB/texas, WebKB/wisconsin')
     # parser.add_argument('--data_path', type=str, default='datasets/', help='data path')
     parser.add_argument('--imb_ratio', type=float, default=100, help='imbalance ratio')
-    parser.add_argument('--net', type=str, default='Cheb', help='GNN bachbone, chosen from GCN, GAT, SAGE, APPNP, Cheb, DIG, SymDiGCN, GIN')
     parser.add_argument('--dropout', type=float, default=0.1, help='dropout prob')
     parser.add_argument('--num_filter', type=int, default=2, help='num of filters')
     parser.add_argument('--layer', type=int, default=2, help='number of layers (2 or 3), default: 2')
@@ -25,7 +26,7 @@ def parse_args():
     parser.add_argument('--feat_dim', type=int, default=64, help='feature dimension')
     parser.add_argument('--warmup', type=int, default=5, help='warmup epoch')
     parser.add_argument('--epoch', type=int, default=1500, help='epoch')
-    parser.add_argument('--lr', type=float, default=100, help='learning rate')
+    parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
     # parser.add_argument('--lr', type=float, default=5e-3, help='learning rate')
     parser.add_argument('--l2', type=float, default=5e-4, help='l2 regularizer')
     parser.add_argument('--tau', type=int, default=2, help='temperature in the softmax function when calculating confidence-based node hardness')
---
+++ b_dgl_CPU.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# List of AugDirect values
+augdirect_values="0 1 -1 2 4 20 21 22 23 231 2311"
+
+Direct_dataset='dgl/citeseer'  # Update your Direct_dataset value
+Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
+IsDirData=False
+unDirect_data='CiteSeer'
+net='Cheb'
+
+generate_timestamp() {
+  date +"%d%H%M%S"
+}
+timestamp=$(generate_timestamp)
+
+logfile="outfor2.log"
+exec > $logfile 2>&1
+
+# Iterate over each AugDirect value
+for augdirect in $augdirect_values; do
+ 
+  nohup python3 main.py --AugDirect=$augdirect --net=$net \
+    --Direct_dataset="$Direct_dataset" --undirect_dataset=$unDirect_data \
+    > ${net}_${Direct_dataset_filename}_Aug${augdirect}_T${timestamp}.log &
+done
+
+# Optionally, wait for all background processes to finish
+#wait
+
