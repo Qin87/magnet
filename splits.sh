6de4f0fbd13e6daaa4d8ff750e8336789f386855
Qin87 <qinjiang19871018@gmail.com>
2024-03-04 18:30:37 +0000

    torch for base

--- splits.sh
+++ splits.sh
@@ -1,6 +1,6 @@
 for log_file in $(ls -1   *.log | sort); do
   echo "     "
-  grep -E '^(AP|GI|Ch|F|S|Di|GC|M)' "$log_file" 
+  grep -E '^(AP|GI|Ch|F|S|Di|GC|M|a|A)' "$log_file" 
  #grep -v '^[eEtNA]' "$log_file"
   #tail -n 2 "$log_file"
 done
