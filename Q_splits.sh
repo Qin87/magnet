73b27fe5ddb5d34681a4f9dcfcb4409c42de901e
Qin87 <qinjiang19871018@gmail.com>
2024-04-13 16:18:09 +0100

    wrong DiGCN_IB_2MixBN_SymCat_nhid

---
+++ Q_splits.sh
@@ -0,0 +1,7 @@
+for log_file in $(ls -1   Aug0*layer3*.log | sort); do
+  echo "     "
+ # grep -E '^(AP|GI|F|S|Di|GC|M|a|A)' "$log_file" 
+ #grep -v '^[eEtNA]' "$log_file"
+  tail -n 2 "$log_file"
+done
+
