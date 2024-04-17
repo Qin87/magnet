b1d7d38a2e4f09894610cffead8c10ce37a9b320
Qin87 <qinjiang19871018@gmail.com>
2024-04-17 18:09:12 +0100

    dmog install

--- nested_net.sh
+++ nested_net.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # List of AugDirect values
-#augdirect_values="2 4 20 21 22 23 231 2311"
-#net_values="addSym Sym addSympara" DiG DiGib DiGSymib DiGSymCatib DiGSymCatMixib DiGSymCatMixSymib  DiGub DiGi3
 net_values=" DiGi4 DiGu3 DiGu4
 addSym Sym addSympara
 Mag MagQin Sig Qua
@@ -10,10 +8,8 @@ GCN GAT APPNP GIN Cheb SAGE
 JKNet pgnn mlp sgc"
 layer_values=" 5 6 7"
 
-Direct_dataset='dgl/cora'  # Update your Direct_dataset value
+Direct_dataset='WebKB/Cornell'  # Update your Direct_dataset value
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
-IsDirData=False
-unDirect_data='CiteSeer'
 
 generate_timestamp() {
   date +"%d%H%Ms%S"
