8a9c1e6c622990b26c3eec95cee3b73fd8efcf80
Qin87 <qinjiang19871018@gmail.com>
2024-04-29 17:37:16 +0100

    time print

--- All2MainStop.py
+++ All2MainStop.py
@@ -866,6 +866,7 @@ try:
                 end_time = time.time()
                 print('epoch: {:3d}, val_loss:{:2f}, acc: {:.2f}, bacc: {:.2f}, tmp_test_f1: {:.2f}, f1: {:.2f}'.format(epoch, val_loss, test_acc * 100, test_bacc * 100, tmp_test_f1*100, test_f1 * 100))
                 print(end_time - start_time, file=log_file)
+                print(end_time - start_time)
                 print('epoch: {:3d}, val_loss:{:2f}, acc: {:.2f}, bacc: {:.2f}, tmp_test_f1: {:.2f}, f1: {:.2f}'.format(epoch, val_loss, test_acc * 100, test_bacc * 100, tmp_test_f1*100, test_f1 * 100),file=log_file)
                 end_epoch = epoch
                 if CountNotImproved > args.NotImproved:
