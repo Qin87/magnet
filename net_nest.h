07429c8d0e1c8fd5665e2ac05b4f365cefc62f9e
Qin87 <qinjiang19871018@gmail.com>
2024-05-06 17:28:05 +0100

    omen

--- net_nest.h
+++ net_nest.h
@@ -7,11 +7,11 @@
 #Mag MagQin Sig Qua
 #GCN GAT APPNP GIN Cheb SAGE
 #JKNet pgnn mlp sgc"Cheb MagQin DiGSymib DiGSymCatib
-net_values="GAT Cheb SAGE GCN GIN Sig  "
+net_values="DiG  "
 
-layer_values=" 4 "
+layer_values=" 2 3 4 "
 
-Direct_dataset='WebKB/texas'  # Update your Direct_dataset value
+Direct_dataset='cora_ml/'  # Update your Direct_dataset value
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 
 generate_timestamp() {
@@ -25,9 +25,9 @@ for layer in $layer_values; do
     exec > $logfile 2>&1  # Redirect stdout and stderr to log file
   # Iterate over each layer value
   for net in $net_values; do
-    nohup python3 All2MainStop.py --AugDirect=0 --net=$net --MakeImbalance\
+    nohup python3 All2MainStop.py --AugDirect=0 --net=$net --MakeImbalance  --to_undirected\
     --layer=$layer  --q=0  --Direct_dataset="$Direct_dataset"  \
-      > ${Direct_dataset_filename}imBala_T${timestamp}_Aug0${net}_layer${layer}.log &
+      > ${Direct_dataset_filename}Imbala_True${timestamp}_Aug0${net}_layer${layer}.log &
     pid=$!
 
     wait $pid
