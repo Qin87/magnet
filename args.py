0a3f055d92a2f53b6ddbafa1d3c34e1c3daa35c9
Qin87 <qinjiang19871018@gmail.com>
2024-05-12 00:39:14 +0100

    dd

--- args.py
+++ args.py
@@ -15,8 +15,8 @@ def parse_args():
     parser.add_argument('--batch_size', type=int, default=1024, help='batch size to train large graph')
     parser.add_argument('--largeData', '--large', action='store_true', help='train in batches for large graph')
 
-    parser.add_argument('--IsDirectedData', action='store_true', help='The dataset is a directed graph')
-    # parser.add_argument('--IsDirectedData', type=bool, default=True, help='the dataset is directed graph')
+    # parser.add_argument('--IsDirectedData', action='store_true', help='The dataset is a directed graph')
+    parser.add_argument('--IsDirectedData', type=bool, default=True, help='the dataset is directed graph')
 
     parser.add_argument('--ibx1', action='store_true', help='share the same ibx block in DiGSymCatib')
     parser.add_argument('--paraD', action='store_true', help='ib is weighted sum')     # TODO false
--- net_nest.h
+++ net_nest.h
@@ -7,8 +7,8 @@
 #Mag MagQin Sig Qua
 #GCN GAT APPNP GIN Cheb SAGE
 #JKNet pgnn mlp sgc"Cheb MagQin DiGSymib DiGSymCatib
-net_values="Mag MagQin "
-q_value=0
+net_values="MagQin "
+q_value=0.5
 Aug_value=0
 
 layer_values="1 2 3 4 5 "
@@ -20,7 +20,7 @@ generate_timestamp() {
   date +"%d%H%Ms%S"
 }
 timestamp=$(generate_timestamp)
-
+.5
 # Iterate over each net value
 for layer in $layer_values; do
   logfile="outforlayer${layer}.log"  # Adjust log file name with layer number
@@ -28,7 +28,7 @@ for layer in $layer_values; do
   # Iterate over each layer value
   for net in $net_values; do
     nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net --MakeImbalance \
-    --layer=$layer  --q=$q_value  --Direct_dataset="$Direct_dataset"  \
+    --layer=$layer  --q=$q_value  --Direct_dataset="$Direct_dataset"  --undirect_dataset="$unDirect_data" \
       > ${unDirect_data}Imbala_${timestamp}_Aug${Aug_value}${net}_layer${layer}q${q_value}.log &
     pid=$!
 
