ec6e001237b1d847c5d597ef52222b8904fb580f
Qin87 <qinjiang19871018@gmail.com>
2024-05-16 18:15:12 +0100

    dell

--- net_nest.h
+++ net_nest.h
@@ -7,13 +7,13 @@
 #Mag MagQin Sig Qua
 #GCN GAT APPNP GIN Cheb SAGE
 #JKNet pgnn mlp sgc"Cheb MagQin DiGSymib DiGSymCatib
-net_values="GAT "
-q_value=0.5
+net_values="Mag MagQin Sig "
+q_value=0.25
 Aug_value=0
 
 layer_values="1 2 3 4 5 "
 
-Direct_dataset='citeseer_npz/'  # Update your Direct_dataset value
+Direct_dataset='telegram/telegram'  # Update your Direct_dataset value
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 unDirect_data='Coauthor-CS'
 generate_timestamp() {
@@ -27,7 +27,7 @@ for layer in $layer_values; do
     exec > $logfile 2>&1  # Redirect stdout and stderr to log file
   # Iterate over each layer value
   for net in $net_values; do
-    nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net    --to_undirected    --all1\
+    nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net    --to_undirected   --IsDirectedData \
     --layer=$layer  --q=$q_value  --Direct_dataset="$Direct_dataset"  --undirect_dataset="$unDirect_data" \
       > wrongname_${Direct_dataset_filename}Bala_Undirect_${timestamp}_Aug${Aug_value}${net}_layer${layer}q${q_value}.log &
     pid=$!
