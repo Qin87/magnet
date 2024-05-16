2d1143c9b5ce9d4f2c0be29667938d0a483a4dfe
Qin87 <qinjiang19871018@gmail.com>
2024-05-16 10:16:30 +0100

    dell

--- net_nest.h
+++ net_nest.h
@@ -1,21 +1,13 @@
 #!/bin/bash
-<<<<<<< HEAD
 
 # List of AugDirect values --MakeImbalance
-=======
-# List of AugDirect values
->>>>>>> 8cb3c604773d86dfbd10ef97fded50af2300caf7
 #net_values="DiGSymib DiGSymCatib Qua Sig DiG DiGib DiGSymib DiGSymCatib DiGSymCatMixib"
 # DiGi4 DiGu3 DiGu4
 #addSym Sym addSympara
 #Mag MagQin Sig Qua
 #GCN GAT APPNP GIN Cheb SAGE
 #JKNet pgnn mlp sgc"Cheb MagQin DiGSymib DiGSymCatib
-<<<<<<< HEAD
-net_values="DiGSymCatMixib Sig Mag MagQin DiGub DiGSymCatib"
-=======
-net_values="DiGSymib "
->>>>>>> 8cb3c604773d86dfbd10ef97fded50af2300caf7
+net_values="GCN "
 q_value=0.5
 Aug_value=0
 
@@ -23,7 +15,7 @@ layer_values="1 2 3 4 5 "
 
 Direct_dataset='citeseer_npz/'  # Update your Direct_dataset value
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
-unDirect_data='PubMed'
+unDirect_data='CiteSeer'
 generate_timestamp() {
   date +"%d%H%Ms%S"
 }
@@ -35,9 +27,9 @@ for layer in $layer_values; do
     exec > $logfile 2>&1  # Redirect stdout and stderr to log file
   # Iterate over each layer value
   for net in $net_values; do
-    nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net  --IsDirectedData   --to_undirected\
+    nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net    --to_undirected   --MakeImbalance  --all1\
     --layer=$layer  --q=$q_value  --Direct_dataset="$Direct_dataset"  --undirect_dataset="$unDirect_data" \
-      > ${Direct_dataset_filename}Bala_Undirect_${timestamp}_Aug${Aug_value}${net}_layer${layer}q${q_value}.log &
+      > wrongname_${Direct_dataset_filename}Bala_Undirect_${timestamp}_Aug${Aug_value}${net}_layer${layer}q${q_value}.log &
     pid=$!
 
     wait $pid
