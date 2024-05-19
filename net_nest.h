22f8d89deeb318608e19c949c5e3806e84c9f3d9
Qin87 <qinjiang19871018@gmail.com>
2024-05-19 20:30:06 +0100

    omen

--- net_nest.h
+++ net_nest.h
@@ -11,9 +11,9 @@ net_values=" DiGSymib DiGSymCatib Qua  DiG DiGib DiGub DiGSymCatMixib addSym Sym
 q_value=0.25
 Aug_value=0
 
-layer_values="2 3 4 5 "
+layer_values=" 3 4 5 "
 
-Direct_dataset='WebKB/cornell'  # Update your Direct_dataset value
+Direct_dataset='cora_ml/'  # Update your Direct_dataset value
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 unDirect_data='CiteSeer'
 generate_timestamp() {
@@ -27,7 +27,7 @@ for layer in $layer_values; do
     exec > $logfile 2>&1  # Redirect stdout and stderr to log file
   # Iterate over each layer value
   for net in $net_values; do
-    nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net       \
+    nohup python3 All2MainStop.py --AugDirect=$Aug_value --net=$net   --IsDirectedData    \
     --layer=$layer  --q=$q_value  --Direct_dataset="$Direct_dataset"  --undirect_dataset="$unDirect_data" \
       > wrongname_${Direct_dataset_filename}Bala_Undirect_${timestamp}_Aug${Aug_value}${net}_layer${layer}q${q_value}.log &
     pid=$!
