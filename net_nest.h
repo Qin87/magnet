cffb288e0f6eed2c7f2a3bc3c9bdca79f26e72ad
Qin87 <qinjiang19871018@gmail.com>
2024-06-09 19:01:38 +0100

    dell

--- net_nest.h
+++ net_nest.h
@@ -7,14 +7,14 @@
 #Mag MagQin Sig Qua
 #GCN GAT APPNP GIN Cheb SAGE
 #JKNet pgnn mlp sgc"Cheb MagQin  DiGSymib DiGSymCatib  DiG DiGib  DiGSymCatMixib DiGSymCatMixSymib
-net_values="DiG DiGib"
+net_values="GCN"
 q_value=0.5
 Aug_value=0
-layer_values=" 1 2  3 4  "    #
+layer_values="  4 5 6 7 8  "    #
 
 Direct_dataset=( 'telegram/telegram' )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin' 'WebKB/texas'
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
-unDirect_data='Cora'
+unDirect_data='CiteSeer'
 generate_timestamp() {
   date +"%d%H%Ms%S"
 }
@@ -28,7 +28,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             exec > $logfile 2>&1  # Redirect stdout and stderr to log file
           # Iterate over each layer value
           for net in $net_values; do
-            nohup python3 All2MainStop.py --AugDirect=$Aug --net=$net   --W_degree=1   --IsDirectedData  --to_undirected   --MakeImbalance\
+            nohup python3 All2MainStop.py --AugDirect=$Aug --net=$net   --W_degree=1  \
             --layer=$layer  --q=$q_value  --Direct_dataset="$Didataset"  --undirect_dataset="$unDirect_data" \
               > wrongname_${Direct_dataset_filename}Bala_Undirect_${timestamp}_Aug${Aug}${net}_layer${layer}q${q_value}.log &
             pid=$!
