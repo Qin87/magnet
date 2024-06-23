3876322a8c5743c8b8174fba02092faaa95b4f38
Qin87 <qinjiang19871018@gmail.com>
2024-06-23 10:04:07 +0100

    dell

--- net_nest.h
+++ net_nest.h
@@ -7,13 +7,13 @@
 #Mag MagQin Sig Qua
 #GCN GAT APPNP GIN Cheb SAGE  220 230 -2  0 1 -1 2 21 20  22 23 4
 #JKNet pgnn mlp sgc"Cheb MagQin  DiGSymib DiGSymCatib  DiG DiGib  DiGSymCatMixib DiGSymCatMixSymib GAT GCN  SAGE  # QiGi4
-net_values="  WiG WiGib WiGi3"
+net_values=" DiG DiGib "
 q_value=0
 Aug_value=" 0 "
-layer_values=" 4   "    #
+layer_values="2 3  4   "    #
 
 
-Direct_dataset=( 'cora_ml/' )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
+Direct_dataset=( 'telegram/telegram' )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 unDirect_data='Cora'
 generate_timestamp() {
@@ -29,7 +29,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             exec > $logfile 2>&1  # Redirect stdout and stderr to log file
           # Iterate over each layer value
           for net in $net_values; do
-            nohup python3 All2MainStop.py --AugDirect=$Aug --net=$net   --W_degree=400      --IsDirectedData    \
+            nohup python3 All2MainStop.py --AugDirect=$Aug --net=$net   --W_degree=300      --IsDirectedData    \
             --layer=$layer  --q=$q_value  --Direct_dataset="$Didataset"  --undirect_dataset="$unDirect_data" \
               > wrongname_${Direct_dataset_filename}Bala_Undirect_${timestamp}_Aug${Aug}${net}_layer${layer}q${q_value}.log &
             pid=$!
