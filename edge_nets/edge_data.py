*ym wrong for edge_weight normalization

edge_nets/edge_data.py
@@ -1140,12 +1140,12 @@ def normalize_edges(edge_index, edge_weight, num_nodes, dtype=torch.float):
 
     return deg_inv_sqrt[row] * edge_weight * deg_inv_sqrt[col]
 
-def normalize_edges_all1(edge_index, dtype=torch.float):
+def normalize_edges_all1(num_nodes, edge_index, dtype=torch.float):
     device = edge_index.device
     edge_weight = torch.ones((edge_index.size(1),), dtype=dtype, device=device)
 
     row, col = edge_index
-    deg = scatter_add(edge_weight, row, dim=0, dim_size=edge_index.size(1))
+    deg = scatter_add(edge_weight, row, dim=0, dim_size=num_nodes)
     deg_inv_sqrt = deg.pow(-0.5)
     deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
 
@@ -1155,6 +1155,7 @@ def Qin_get_second_directed_adj(edge_index, num_nodes, dtype, k):     #
     device = edge_index.device
     fill_value = 1
     # edge_index, _ = add_self_loops(edge_index.long(), fill_value=fill_value, num_nodes=num_nodes)       # TODO add back after no-selfloop test
+    edge_index, _ = remove_self_loops(edge_index)
     edge_index = edge_index.to(device)
 
     edge_weight = torch.ones(edge_index.size(1), dtype=torch.bool).to(device)
@@ -1166,7 +1167,7 @@ def Qin_get_second_directed_adj(edge_index, num_nodes, dtype, k):     #
     for L in L_tuple:  # Skip L1 if not needed
         edge_indexL = L._indices()
         all_hop_edge_index.append(edge_indexL)
-        edge_weightL = normalize_edges_all1(edge_indexL).to(device)
+        edge_weightL = normalize_edges_all1(num_nodes, edge_indexL).to(device)
         all_hops_weight.append(edge_weightL)
 
     return tuple(all_hop_edge_index), tuple(all_hops_weight)
@@ -1488,7 +1489,7 @@ def get_second_directed_adj_union(edge_index, num_nodes, dtype, k):
     all_hops_weight = []
     for L in L_tuple:  # Skip L1 if not needed
         edge_indexL = L._indices()
-        edge_weightL = normalize_edges_all1(edge_indexL)
+        edge_weightL = normalize_edges_all1(num_nodes, edge_indexL)
         all_edge_index.append(edge_indexL)
         all_hops_weight.append(edge_weightL)
 

main.py
@@ -20,7 +20,7 @@ from edge_nets.Edge_DiG_ import edge_prediction
 from edge_nets.edge_data import get_appr_directed_adj, get_second_directed_adj, get_second_directed_adj_union, \
      WCJ_get_directed_adj, Qin_get_second_directed_adj, Qin_get_directed_adj, get_appr_directed_adj2, Qin_get_second_directed_adj0
 from data_model import CreatModel, load_dataset, log_file, get_name
-from nets.DiG_NoConv import union_edges, last_edges
+from nets.DiG_NoConv import last_edges
 from nets.src2 import laplacian
 from nets.src2.quaternion_laplacian import process_quaternion_laplacian
 from preprocess import  F_in_out,  F_in_out_Qin,  F_in_out0
@@ -70,7 +70,7 @@ def train(train_idx, edge_in, in_weight, edge_out, out_weight, SparseEdges, edge
           X_img_i, X_img_j, X_img_k,norm_img_i,norm_img_j, norm_img_k, Quaedge_index):
     # print("come to train")
 
-    global class_num_list, idx_info, prev_out
+    global class_num_list, idx_info, prev_out, biedges
     global data_train_mask, data_val_mask, data_test_mask
     new_edge_index=None
     new_x = None
@@ -175,6 +175,7 @@ torch.backends.cudnn.qinchmark = False
 random.seed(seed)
 np.random.seed(seed)
 
+biedges = None
 edge_in = None
 in_weight = None
 edge_out = None
@@ -315,7 +316,7 @@ try:
                 if args.net.startswith('ym'):
                     print('Sym edge size(biedge, edge_in, edge_out):', biedges.size(),  in_weight.size(),  out_weight.size(), file=log_file)
                     print('Sym edge size(biedge, edge_in, edge_out):', biedges.size(),  in_weight.size(),  out_weight.size())
-                elif args.net[1:].startswith('i'):
+                elif args.net[1:].startswith('i') and isinstance(edge_weight, tuple):
                     print('edge size:', end=' ', file=log_file)
                     print('edge size:', end=' ')
                     for i in edge_weight:

nets/DiG_NoConv.py
@@ -73,19 +73,19 @@ class InceptionBlock_Di(torch.nn.Module):
         return x0
 
 
-def union_edges(edge_index_tuple):
-    concatenated_tensor = torch.cat(edge_index_tuple, dim=1)
-    edges_tuples = list(set(zip(concatenated_tensor[0].tolist(), concatenated_tensor[1].tolist())))
-    union_tensor = torch.tensor(edges_tuples).T
-    unique_weights = normalize_edges_all1(union_tensor)
-
-    print('result:', unique_weights.size())
-    for i in range(len(edge_index_tuple)):
-        print(edge_index_tuple[i].size()[1], end=', ')
-        if unique_weights.size() == edge_index_tuple[i].size()[1]:
-            print("####### Only the ", i+1, "th element in edge_weight_tuple is used.")
-
-    return union_tensor, unique_weights
+# def union_edges(edge_index_tuple):
+#     concatenated_tensor = torch.cat(edge_index_tuple, dim=1)
+#     edges_tuples = list(set(zip(concatenated_tensor[0].tolist(), concatenated_tensor[1].tolist())))
+#     union_tensor = torch.tensor(edges_tuples).T
+#     unique_weights = normalize_edges_all1(union_tensor)
+#
+#     print('result:', unique_weights.size())
+#     for i in range(len(edge_index_tuple)):
+#         print(edge_index_tuple[i].size()[1], end=', ')
+#         if unique_weights.size() == edge_index_tuple[i].size()[1]:
+#             print("####### Only the ", i+1, "th element in edge_weight_tuple is used.")
+#
+#     return union_tensor, unique_weights
 
 def last_edges(edge_index_tuple):
     # concatenated_tensor = torch.cat(edge_index_tuple, dim=1)

preprocess.py
@@ -429,6 +429,7 @@ def F_in_out(edge_index, size, edge_weight=None):
 
     # Convert the sparse tensor to a dense tensor for multiplication
     a_dense = a_tensor.to_dense()
+    num_nodes = a_dense.size(0)
 
     A_in = torch.mm(a_dense.T, a_dense)
     A_out = torch.mm(a_dense, a_dense.T)
@@ -443,8 +444,8 @@ def F_in_out(edge_index, size, edge_weight=None):
     # out_weight = torch.from_numpy(A_out.data).float().to(device)
     # in_weight = torch.ones(edge_in.size(1)).to(device)
     # out_weight = torch.ones(edge_out.size(1)).to(device)
-    in_weight = normalize_edges_all1(edge_in)
-    out_weight = normalize_edges_all1(edge_out)
+    in_weight = normalize_edges_all1(num_nodes, edge_in)
+    out_weight = normalize_edges_all1(num_nodes, edge_out)
 
     edge_index = edge_index.to(device)  # Ben GPU
     return to_undirected(edge_index), edge_in, in_weight, edge_out, out_weight