3916618607bcff5455c2cf28af08c0784271fa3a
Qin87 <qinjiang19871018@gmail.com>
2024-07-17 11:59:51 +0100

    Li and Ui to experiment on other devices

--- net_nest.h
+++ net_nest.h
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # CiG  CiGi2 CiGi3  CiGu2 CiGu3 CiGi4 CiGu4
-net_values="LiGi2 LiGi3 LiGi4 LiGu2 LiGu3 LiGu4 LiG"
+# LiGi2 LiGi3 LiGi4 LiGu2 LiGu3 LiGu4 LiG
+net_values="QiGi2 QiGi3 QiGu2 QiGu3 QiGu4 QiGi4"
 q_value=0
 layer_values="1 2 3 4 "    #
 
 # 'Cora/' 'CiteSeer/' 'PubMed/' 'dgl/photo' 'dgl/computer' 'dgl/reddit' 'dgl/coauthor-cs' 'dgl/coauthor-ph' 'dgl/Fyelp' 'dgl/Famazon'
 # 'citeseer_npz/' 'cora_ml/'  'telegram/telegram'  'citeseer_npz/' 'cora_ml/'  'telegram/telegram' 'dgl/pubmed' 'dgl/cora' 'WikiCS/'
-Direct_dataset=( 'dgl/pubmed' 'citeseer_npz/' 'cora_ml/'  'telegram/telegram' 'dgl/cora' 'WikiCS/' )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
+Direct_dataset=( 'telegram/telegram')  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 generate_timestamp() {
   date +"%d%H%Ms%S"
@@ -23,7 +24,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             log_output="QymNorm_NoSelfLoop_${Didataset//\//_}_${timestamp}_${net}_layer${layer}q${q_value}.log"
 
             # Run the Python script with parameters and log output
-            python3 main.py --net="$net" --W_degree=5 --layer="$layer" --q="$q_value" --Dataset="$Didataset" > "$log_output" 2>&1
+            python3 main.py  --net="$net" --W_degree=5 --layer="$layer" --q="$q_value" --Dataset="$Didataset" > "$log_output" 2>&1
             wait $pid
           done
         done
