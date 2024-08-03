e0463a8083ba3a1c40d6a6a344b5b74e3d8c6ef9
Qin87 <qinjiang19871018@gmail.com>
2024-08-03 14:06:30 +0100

    wikics

--- coraml.h
+++ coraml.h
@@ -8,7 +8,7 @@ Dir="0 0.5 1 -1"
 
 # 'Cora/' 'CiteSeer/' 'PubMed/' 'dgl/photo' 'dgl/computer' 'dgl/reddit' 'dgl/coauthor-cs' 'dgl/coauthor-ph' 'dgl/Fyelp' 'dgl/Famazon'
 # 'citeseer_npz/' 'cora_ml/'  'telegram/telegram'  'citeseer_npz/' 'cora_ml/'  'telegram/telegram' 'dgl/pubmed' 'dgl/cora' 'WikiCS/'
-Direct_dataset=( 'WikiCS/'  )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
+Direct_dataset=( 'citeseer_npz/'  )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 generate_timestamp() {
   date +"%d%H%Ms%S"
@@ -20,20 +20,21 @@ for Didataset in "${Direct_dataset[@]}"; do
     for layer in $layer_values; do
         logfile="outforlayer${layer}.log"
         exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
-        #for alphadir in $Dir; do
+        for alphadir in $Dir; do
         for betadir in $Dir; do
-        #for gamadir in $Dir; do
+        for gamadir in $Dir; do
 # //        for imba_value  in $imbal; do
         for net in $net_values; do
             log_output="${Didataset//\//_}_${timestamp}_A${a}_Dir${betadir}__${net}_layer${layer}q${q_value}.log"
 
             # Run the Python script with parameters and log output
-python3 main.py   --jk='max' --differ_AAt --BN_model=1  --First_self_loop='remove' --lr=0.01 --dropout=0.5 --betaDir="$betadir"   --net="$net"  --layer="$layer"   --Dataset="$Didataset" > "$log_output"
+python3 main.py   --jk='max'  --BN_model=1  --First_self_loop='add' --lr=0.01 --dropout=0.5 --alphaDir="$alphadir" --betaDir="$betadir" --gamaDir="$gamadir" \
+ --net="$net"  --layer="$layer"   --Dataset="$Didataset" > "$log_output" \
              2>&1
             wait $pid
           #done
-        #done
-       #done
+        done
+       done
        done
        done
        done
