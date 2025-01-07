dbd0e2276ea912c72bc738bf1f96fdc2264070bc
Qin87 <qinjiang19871018@gmail.com>
2025-01-07 12:57:36 +0000

    layer test_dmog

--- batch.h
+++ batch.h
@@ -44,10 +44,10 @@ mkdir -p "$RESULTS_DIR"
 # Customize this section to suit your needs.
 net_values="QiGi2 QiGi3 QiGu3 QiGu2 QiGu4 QiGi4"
 q_value=0
-layer_values="1 2 3  4  "    #
+layer_values=" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
 
 # 'citeseer_npz/' 'cora_ml/'  'telegram/telegram'
-Direct_dataset=( 'citeseer_npz/' 'cora_ml/'  'telegram/telegram' 'dgl/pubmed' 'dgl/cora' 'WikiCS/'  )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
+Direct_dataset=( 'citeseer_npz/' 'cora_ml/'  'telegram/telegram'  )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 generate_timestamp() {
   date +"%d%H%Ms%S"
@@ -59,11 +59,16 @@ for Didataset in "${Direct_dataset[@]}"; do
     for layer in $layer_values; do
         logfile="outforlayer${layer}.log"
         exec > "$logfile" 2>&1  # Redirect stdout and stderr to log file
+        # for imba_value  in $imbal; do
         for net in $net_values; do
-            # Run the Python script with parameters and log output
-            python3 main.py --net="$net"  --W_degree=5  --layer="$layer" --q="$q_value" --Dataset="$Didataset" > "NormQym_NoSelfLoop_${Didataset//\//_}_${timestamp}_${net}_layer${layer}q${q_value}.log"
+            log_output="${Didataset//\//_}_${timestamp}_${net}_layer${layer}q${q_value}.log"
 
+            # Run the Python script with parameters and log output
+            python3 main.py     --layer="$layer"   \
+            --Dataset="$Didataset" > "$log_output"
+             2>&1
             wait $pid
-          done
+          # done
+        done
         done
 done
--- net_nest.h
+++ net_nest.h
@@ -3,8 +3,6 @@
 net_values="Dir-GNN "
 q_value=0
 layer_values=" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
-imbal="100  "
-
 
 # 'citeseer/' 'cora_ml/'  'telegram/'   'dgl/pubmed'  'WikiCS/'  'WikipediaNetwork/chameleon' 'WikipediaNetwork/squirrel'   --net="$net"
 Direct_dataset=(   'citeseer/' 'cora_ml/'  'telegram/' )
