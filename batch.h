37e89c7918bf7daa72ff592a9f48da6b6b2d920a
Qin87 <qinjiang19871018@gmail.com>
2025-01-07 13:03:47 +0000

    layer test_dmog

--- batch.h
+++ batch.h
@@ -42,7 +42,7 @@ mkdir -p "$RESULTS_DIR"
 #  Application launch commands
 #-------------------------------
 # Customize this section to suit your needs.
-net_values="QiGi2 QiGi3 QiGu3 QiGu2 QiGu4 QiGi4"
+net_values="Dir-GNN "
 q_value=0
 layer_values=" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
 
