43f5f1dafbb5d3131b99bac865575256d4a95ca7
Qin87 <qinjiang19871018@gmail.com>
2025-01-10 22:30:36 +0000

    parallel debug

--- batch.h
+++ batch.h
@@ -44,7 +44,7 @@ mkdir -p "$RESULTS_DIR"
 # Customize this section to suit your needs.
 net_values="Dir-GNN "
 q_value=0
-layer_values=" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
+layer_values="  12 13 14 15 16 17 18 19 20  "
 
 # 'citeseer/' 'cora_ml/'  'telegram/telegram'
 Direct_dataset=( 'WikiCS/' )  # 'cora_ml/'  'citeseer_npz/'  'WebKB/Cornell' 'WebKB/wisconsin'  'WebKB/texas' 'WebKB/texas' 'WebKB/wisconsin'  telegram/telegram
