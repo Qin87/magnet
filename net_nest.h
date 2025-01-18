4faf71dba9e92f500e9a6a3a4d6ff1e0c33f1c79
Qin87 <qinjiang19871018@gmail.com>
2025-01-18 15:06:02 +0000

    reverse

--- net_nest.h
+++ net_nest.h
@@ -6,7 +6,7 @@ layer_values="  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20  "
 #layer_values="   1  15 16 17 18 19 20 30 40 50 60 70 "
 
 # 'citeseer/' 'cora_ml/'  'telegram/'   'dgl/pubmed'  'WikiCS/'  'WikipediaNetwork/chameleon' 'WikipediaNetwork/squirrel'   --net="$net"
-Direct_dataset=(  'cora_ml/'  'telegram/'  )
+Direct_dataset=(  'dgl/pubmed'  )
 Direct_dataset_filename=$(echo $Direct_dataset | sed 's/\//_/g')
 generate_timestamp() {
   date +"%d%H%Ms%S"
@@ -23,7 +23,7 @@ for Didataset in "${Direct_dataset[@]}"; do
             log_output="${Didataset//\//_}_${timestamp}_${net}_layer${layer}q${q_value}.log"
 
             # Run the Python script with parameters and log output
-            python3 main.py     --Ak="$layer"   \
+            python3 main.py     --layer="$layer"   \
             --Dataset="$Didataset" > "$log_output"
              2>&1
             wait $pid
