91cdff57de4f8a057b3d812eeb9f09f6d699da45
Qin87 <qinjiang19871018@gmail.com>
2025-02-14 13:04:08 +0000

    gpu

--- nets/gcn.py
+++ nets/gcn.py
@@ -91,42 +91,42 @@ def norm0(edge_index, edge_weight=None, num_nodes=None, improved=False,
         edge_weight = row_deg_inv_sqrt[row] * edge_weight * col_deg_inv_sqrt[col]
     return edge_index, edge_weight
 
-def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
-             add_self_loops=0, dtype=None):
-
-    fill_value = 2. if improved else 1.
-
-    if isinstance(edge_index, SparseTensor):
-        adj_t = edge_index
-        if not adj_t.has_value():
-            adj_t = adj_t.fill_value(1., dtype=dtype)
-        if add_self_loops == 1:
-            adj_t = fill_diag(adj_t, fill_value)
-        deg = sum(adj_t, dim=1)
-        deg_inv_sqrt = deg.pow_(-0.5)
-        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
-        adj_t = mul(adj_t, deg_inv_sqrt.view(-1, 1))
-        adj_t = mul(adj_t, deg_inv_sqrt.view(1, -1))
-        return adj_t
-
-    else:
-        num_nodes = maybe_num_nodes(edge_index, num_nodes)
-
-        if edge_weight is None:
-            edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
-                                     device=edge_index.device)
-
-        if add_self_loops == 1:
-            edge_index, tmp_edge_weight = add_remaining_self_loops(
-                edge_index, edge_weight, fill_value, num_nodes)
-            assert tmp_edge_weight is not None
-            edge_weight = tmp_edge_weight
-
-        row, col = edge_index[0], edge_index[1]
-        deg = scatter_add(edge_weight, col, dim=0, dim_size=num_nodes)
-        deg_inv_sqrt = deg.pow_(-0.5)
-        deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0)
-        return edge_index, deg_inv_sqrt[col] * edge_weight * deg_inv_sqrt[col]
+# def gcn_norm(edge_index, edge_weight=None, num_nodes=None, improved=False,
+#              add_self_loops=0, dtype=None):
+#
+#     fill_value = 2. if improved else 1.
+#
+#     if isinstance(edge_index, SparseTensor):
+#         adj_t = edge_index
+#         if not adj_t.has_value():
+#             adj_t = adj_t.fill_value(1., dtype=dtype)
+#         if add_self_loops == 1:
+#             adj_t = fill_diag(adj_t, fill_value)
+#         deg = sum(adj_t, dim=1)
+#         deg_inv_sqrt = deg.pow_(-0.5)
+#         deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0.)
+#         adj_t = mul(adj_t, deg_inv_sqrt.view(-1, 1))
+#         adj_t = mul(adj_t, deg_inv_sqrt.view(1, -1))
+#         return adj_t
+#
+#     else:
+#         num_nodes = maybe_num_nodes(edge_index, num_nodes)
+#
+#         if edge_weight is None:
+#             edge_weight = torch.ones((edge_index.size(1), ), dtype=dtype,
+#                                      device=edge_index.device)
+#
+#         if add_self_loops == 1:
+#             edge_index, tmp_edge_weight = add_remaining_self_loops(
+#                 edge_index, edge_weight, fill_value, num_nodes)
+#             assert tmp_edge_weight is not None
+#             edge_weight = tmp_edge_weight
+#
+#         row, col = edge_index[0], edge_index[1]
+#         deg = scatter_add(edge_weight, col, dim=0, dim_size=num_nodes)
+#         deg_inv_sqrt = deg.pow_(-0.5)
+#         deg_inv_sqrt.masked_fill_(deg_inv_sqrt == float('inf'), 0)
+#         return edge_index, deg_inv_sqrt[col] * edge_weight * deg_inv_sqrt[col]
 
 class GCNConv_SHA(MessagePassing):
     r"""GraphSHA
